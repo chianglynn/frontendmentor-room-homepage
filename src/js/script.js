6e1e7e5f6448b93189f93eba07b0aad2a1281491
Chiang Lynn <lynnchiangtw@gmail.com>
2021-09-09 14:16:32 +0800

    Build function for changing slides

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,29 @@
+// Variables
 const mobileNavOpenIcon = document.querySelector('.navigation__icon--hamburger');
 const mobileNavCloseIcon = document.querySelector('.navigation__icon--close');
 const mobileNav = document.querySelector('.navigation__list');
 const modal = document.querySelector('.modal');
+const slidePictures = document.querySelectorAll('.slider__pictures');
+const slideInfos = document.querySelectorAll('.info__slider');
+const prevSlideButton = document.querySelector('.slides-btn--left');
+const nextSlideButton = document.querySelector('.slides-btn--right');
+let currentSlide = 0;
 
-
-
+// Functions
 function toggleMobileNav() {
     [mobileNavCloseIcon, mobileNav, modal].forEach(element => element.classList.toggle('hidden'));
 }
 
+function changeSlide(action) {
+    if (action === 'prev') currentSlide === 0 ? currentSlide = slidePictures.length - 1 : currentSlide--;
+    if (action === 'next') currentSlide === slidePictures.length - 1 ? currentSlide = 0 : currentSlide++;
+    slidePictures.forEach(element => element.style.display = 'none');
+    slideInfos.forEach(element => element.style.display = 'none');
+    slidePictures[currentSlide].style.display = 'flex';
+    slideInfos[currentSlide].style.display = 'block';
+}
+
+// Event listeners
 [mobileNavOpenIcon, mobileNavCloseIcon].forEach(element => element.addEventListener('click', toggleMobileNav));
+prevSlideButton.addEventListener('click', () => changeSlide('prev'));
+nextSlideButton.addEventListener('click', () => changeSlide('next'));
